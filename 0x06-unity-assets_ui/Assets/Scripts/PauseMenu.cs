a130e1a3f24ab606dbdbb6dc3a31e9c578fc79f3
Ayden Townsley <aydentownsley@gmail.com>
2021-09-20 23:22:54 -0500

    adding scripts

---
+++ 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using StarterAssets;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    private bool paused = false;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+        }
+
+        if (paused)
+        {
+            AcvivateMenu();
+        }
+        else
+        {
+            DeactivateMenu();
+        }
+    }
+
+    public void AcvivateMenu()
+    {
+        paused = true;
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+    }
+
+    public void DeactivateMenu()
+    {
+        paused = false;
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void Resume()
+    {
+        paused = false;
+        Debug.Log("Resume");
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ 0x06-unity-assets_ui/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    // private CharacterController controller;
+    // private Vector3 playerVelocity;
+    // private bool groundedPlayer;
+    // public float playerSpeed = 10.0f;
+    // public float jumpHeight = 1.0f;
+    // public float gravityValue = -9.81f;
+    public Vector3 startposition = new Vector3(0, 25, 0);
+    private bool yAxis = false;
+
+    private void Start()
+    {
+        PlayerPrefs.SetString("scene", SceneManager.GetActiveScene().name);
+        if (PlayerPrefs.GetInt("yAxis") == 1)
+            yAxis = true;
+        else
+            yAxis = false;
+
+        GameObject.Find("Freelook").GetComponent<CinemachineFreeLook>().m_YAxis.m_InvertInput = yAxis;
+        // controller = gameObject.AddComponent<CharacterController>();
+        // controller.skinWidth = 0.001f;
+    }
+
+    void FixedUpdate()
+    {
+        // groundedPlayer = controller.isGrounded;
+        // if (groundedPlayer && playerVelocity.y < 0)
+        //     playerVelocity.y = 0f;
+
+        // Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        // controller.Move(move * Time.deltaTime * playerSpeed);
+
+        // if (move != Vector3.zero)
+        // {
+        //     gameObject.transform.forward = move;
+        // }
+
+        // // Changes the height position of the player..
+        // if (Input.GetButton("Jump") && groundedPlayer)
+        // {
+        //     playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        // }
+
+        // playerVelocity.y += gravityValue * Time.deltaTime;
+        // controller.Move(playerVelocity * Time.deltaTime);
+
+        if (transform.position.y < -25)
+        {
+            gameObject.GetComponent<CharacterController>().enabled = false;
+            transform.position = startposition;
+            gameObject.GetComponent<CharacterController>().enabled = true;
+        }
+    }
+
+}
