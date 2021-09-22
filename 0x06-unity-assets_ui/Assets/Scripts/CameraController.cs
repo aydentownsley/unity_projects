e5659efbb49fbd2289b726786dde3ccf7203fc75
Ayden Townsley <aydentownsley@gmail.com>
2021-09-22 13:42:02 -0500

    adding isinverted

--- 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -7,6 +7,7 @@ public class CameraController : MonoBehaviour
     public Transform player;
     Vector2 rotation = Vector2.zero;
     public float speed = 3;
+    public bool isInverted = false;
     // Start is called before the first frame update
     void Start()
     {
